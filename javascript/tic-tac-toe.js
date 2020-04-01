b30655ce677b445a182b1911beb573e63d2b0406
Jaakal <jaak.kivinukk@gmail.com>
2020-04-01 20:05:54 +0300

    Created the board styles and added the functionality for marking the places on the board

--- javascript/tic-tac-toe.js
+++ javascript/tic-tac-toe.js
@@ -1,19 +1,34 @@
 const gameBoard = (() => {
-  let board = [['', '', ''], 
-               ['', '', ''], 
-               ['', '', '']];
+  let board = ['', '', '', 
+               '', '', '', 
+               '', '', ''];
   
   const getBoard = () => {
     return board;
   }
 
-  const setMarker = (marker, row, column) => {
-    if (board[row][column] === '') {
-      board[row][column] = marker;
-      return true;
+  const setMarker = (players, currentPlayer, index) => {
+    if (board[index] === '') {
+      board[index] = currentPlayer.getMarker();
     } else {
-      return false;
+      if (board[index] === currentPlayer.getMarker()) {
+        return `You already have marked that place!`;
+      } else {
+        let otherPlayer;
+
+        for (const player of Object.values(players)) {
+          if (player.getMarker != currentPlayer.getMarker) {
+            otherPlayer = player;
+            break;
+          }
+        }
+
+        return `That place is already marked by ${otherPlayer.getName()}`;
+      }
+
     }
+
+    return false;
   }
 
   const reset = () => {
@@ -24,7 +39,37 @@ const gameBoard = (() => {
     }
   }
 
-  return {setMarker, reset, getBoard}
+  const getBoardHtmlString = () => {
+    let index = 0;
+
+    return `<div class="board">
+          <div class="row row-1">
+              <div class="column column-1" data-index="${index}">${board[0]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-2" data-index="${index += 1}">${board[1]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-3" data-index="${index += 1}">${board[2]}</div>
+          </div>
+          <div class="horizontal-border"></div>
+          <div class="row row-2">
+              <div class="column column-1" data-index="${index += 1}">${board[3]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-2" data-index="${index += 1}">${board[4]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-3" data-index="${index += 1}">${board[5]}</div>
+          </div>
+          <div class="horizontal-border"></div>
+          <div class="row row-3">
+              <div class="column column-1" data-index="${index += 1}">${board[6]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-2" data-index="${index += 1}">${board[7]}</div>
+              <div class="vertical-border"></div>
+              <div class="column column-3" data-index="${index += 1}">${board[8]}</div>
+          </div>
+      </div>`
+  }
+
+  return {setMarker, reset, getBoard, getBoardHtmlString}
 })();
 
 const displayController = (() => {
@@ -52,6 +97,29 @@ const Player = (name, marker) => {
   return {getName, getMarker}
 }
 
+
+
 $(document).ready(() => {
-  displayController.renderBoard();
+  // displayController.renderBoard();
+
+  players = { 
+    player1: Player('John Doe', 'X'),
+    player2: Player('Jane Doe', 'O')
+  }
+  
+  $('body').html(gameBoard.getBoardHtmlString());
+
+
+  const callbackClosureColumn = (event) => {
+    let errorMessage = gameBoard.setMarker(players, players.player1, $(event.target).data('index'));
+    
+    if (errorMessage) {
+      console.log(errorMessage);
+    }
+    
+    $('body').html(gameBoard.getBoardHtmlString());
+    $('.column').click(callbackClosureColumn);
+  };
+  
+  $('.column').click(callbackClosureColumn);
 });
