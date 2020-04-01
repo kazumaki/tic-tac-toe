c48cd014b89b32a6b395e875af6fffba5c5e6840
Jaakal <jaak.kivinukk@gmail.com>
2020-04-02 00:16:35 +0300

    Added enter players from and set the functionalty in place, created styles and refactored code overall.

--- javascript/tic-tac-toe.js
+++ javascript/tic-tac-toe.js
@@ -3,123 +3,113 @@ const gameBoard = (() => {
                '', '', '', 
                '', '', ''];
   
-  let succesfulMoves = 0;
+  let successfulMoves = 0;
 
   const getBoard = () => {
     return board;
   }
 
-  const setMarker = (players, currentPlayer, index) => {
+  const getSuccessfulMoves = () => {
+    return successfulMoves;
+  }
+
+  const setMarker = (index) => {
     if (board[index] === '') {
-      board[index] = currentPlayer.getMarker();
+      board[index] = game.getCurrentPlayer().getMarker();
     } else {
-      if (board[index] === currentPlayer.getMarker()) {
+      if (board[index] === game.getCurrentPlayer().getMarker()) {
         return `You already have marked that place!`;
       } else {
-        let otherPlayer;
-
-        for (const player of Object.values(players)) {
-          if (player.getMarker != currentPlayer.getMarker) {
-            otherPlayer = player;
-            break;
-          }
-        }
-
-        return `That place is already marked by ${otherPlayer.getName()}`;
+        return `That place is already marked by ${game.getOtherPlayer().getName()}`;
       }
-
     }
-    succesfulMoves += 1;
+
+    successfulMoves += 1;
+    
     return false;
   }
 
   const reset = () => {
-    console.log('what');
     for(i = 0; i < 9; i += 1){
       board[i] = '';
     }
+
+    successfulMoves = 0;
   }
 
   const getBoardHtmlString = () => {
     let index = 0;
 
-    return `<div class="board">
-          <div class="row row-1">
-              <div class="column column-1" data-index="${index}">${board[0]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-2" data-index="${index += 1}">${board[1]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-3" data-index="${index += 1}">${board[2]}</div>
-          </div>
-          <div class="horizontal-border"></div>
-          <div class="row row-2">
-              <div class="column column-1" data-index="${index += 1}">${board[3]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-2" data-index="${index += 1}">${board[4]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-3" data-index="${index += 1}">${board[5]}</div>
-          </div>
-          <div class="horizontal-border"></div>
-          <div class="row row-3">
-              <div class="column column-1" data-index="${index += 1}">${board[6]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-2" data-index="${index += 1}">${board[7]}</div>
-              <div class="vertical-border"></div>
-              <div class="column column-3" data-index="${index += 1}">${board[8]}</div>
-          </div>
-      </div>`
+    return `
+      <div class="current-player">${game.getCurrentPlayer().getName()} turn!</div>
+      <div class="board">
+        <div class="row row-1">
+            <div class="column column-1" data-index="${index}">${board[0]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-2" data-index="${index += 1}">${board[1]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-3" data-index="${index += 1}">${board[2]}</div>
+        </div>
+        <div class="horizontal-border"></div>
+        <div class="row row-2">
+            <div class="column column-1" data-index="${index += 1}">${board[3]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-2" data-index="${index += 1}">${board[4]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-3" data-index="${index += 1}">${board[5]}</div>
+        </div>
+        <div class="horizontal-border"></div>
+        <div class="row row-3">
+            <div class="column column-1" data-index="${index += 1}">${board[6]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-2" data-index="${index += 1}">${board[7]}</div>
+            <div class="vertical-border"></div>
+            <div class="column column-3" data-index="${index += 1}">${board[8]}</div>
+        </div>
+      </div>
+      <button class='reset-button'>Reset</button>`
   }
 
-  const gameOver = (currentPlayer) => {
+  return {setMarker, reset, getBoard, getBoardHtmlString, getSuccessfulMoves}
+})();
+
+const game = (() => {
+  let currentPlayer;
+  let otherPlayer;
+
+  const startGame = (players) => {
+    currentPlayer = players.player1;
+    otherPlayer = players.player2;
+  }
+
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  }
+
+  const getOtherPlayer = () => {
+    return otherPlayer;
+  }
+
+  const gameOver = () => {
     const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]] 
+    
     for (const winCondition of winConditions) {
-        if (board[winCondition[0]] === board[winCondition[1]] && board[winCondition[0]] === board[winCondition[2]]
-            && board[winCondition[0]] != '') {
+        if (gameBoard.getBoard()[winCondition[0]] === gameBoard.getBoard()[winCondition[1]] && gameBoard.getBoard()[winCondition[0]] === gameBoard.getBoard()[winCondition[2]]
+            && gameBoard.getBoard()[winCondition[0]] != '') {
           return `${currentPlayer.getName()} won the game!`;
         }
     }
-    if (succesfulMoves == 9) {
+
+    if (gameBoard.getSuccessfulMoves() == 9) {
       return 'The game is tied'
     }
 
+    [currentPlayer, otherPlayer] = [otherPlayer, currentPlayer];
+    
     return false;
   }
 
-  const winRows = (currentPlayer) => {
-    row1 = [board[0], board[1], board[2]].every(marker => {
-      return marker == currentPlayer.getMarker();
-    })
-    row2 = [board[3], board[4], board[5]].every(marker => {
-      return marker == currentPlayer.getMarker();
-    })
-    row3 = [board[6], board[7], board[8]].every(marker => {
-      return marker == currentPlayer.getMarker();
-    })
-
-    return row1 || row2 || row3;
-  }
-  
-
-  return {setMarker, reset, getBoard, getBoardHtmlString, gameOver}
-})();
-
-const displayController = (() => {
-  const renderPlayerForm = () => {
-
-  }
-  const renderBoard = () => {
-    let htmlString = '';
-    for (let i = 0; i < 3; i += 1){
-      for (let j = 0; j < 3; j += 1){
-        htmlString += gameBoard.getBoard()[i][j];
-      }
-      htmlString += '<br>';
-    }
-    $('body').html(htmlString);
-    console.log(htmlString);
-  }
-
-  return {renderBoard}
+  return {startGame, getCurrentPlayer, getOtherPlayer, gameOver};
 })();
 
 const Player = (name, marker) => {
@@ -128,40 +118,52 @@ const Player = (name, marker) => {
   return {getName, getMarker}
 }
 
-
-
-$(document).ready(() => {
-  players = { 
-    player1: Player('John Doe 1', 'X'),
-    player2: Player('Jane Doe 2', 'O')
-  }
-
-  let currentPlayer = players.player1;
+const callbackClosureColumn = (event) => {
+  let errorMessage = gameBoard.setMarker($(event.target).data('index'));
   
-  $('.board-wrapper').html(gameBoard.getBoardHtmlString());
-
-
-  const callbackClosureColumn = (event) => {
-    let errorMessage = gameBoard.setMarker(players, currentPlayer, $(event.target).data('index'));
-    
-    if (errorMessage) {
-      alert(errorMessage);
-    }else{
-      if (gameBoard.gameOver(currentPlayer)) {
-        alert(gameBoard.gameOver(currentPlayer));
-      }
-      currentPlayer = currentPlayer.getMarker() == players.player1.getMarker() ? players.player2 : players.player1;
+  if (errorMessage) {
+    alert(errorMessage);
+  } else {
+    if (game.gameOver()) {
+      $('.board-wrapper').html(gameBoard.getBoardHtmlString())
+      setTimeout(function(){
+        alert(game.gameOver()); 
+        gameBoard.reset();
+        $('.board-wrapper').html(gameBoard.getBoardHtmlString());
+        $('.reset-button').click(callbackClosureReset);
+        $('.column').click(callbackClosureColumn);
+      }, 100);
+    } else {
       $('.board-wrapper').html(gameBoard.getBoardHtmlString());
+      $('.reset-button').click(callbackClosureReset);
       $('.column').click(callbackClosureColumn);
     }
-  };
-  
-  const callbackClosureReset = () => {
-    gameBoard.reset();
-    $('.board-wrapper').html(gameBoard.getBoardHtmlString());
-    $('.column').click(callbackClosureColumn);
+    
   }
+};
 
-  $('.reset-button').click(callbackClosureReset)
+const callbackClosureReset = () => {
+  gameBoard.reset();
+  $('.board-wrapper').html(gameBoard.getBoardHtmlString());
+  $('.reset-button').click(callbackClosureReset);
   $('.column').click(callbackClosureColumn);
+}
+
+$(document).ready(() => {
+  $(".enter-players").submit(function(event) {
+    $('.enter-players').addClass('hide-form');
+
+    players = { 
+      player1: Player($(this).serializeArray()[0].value, 'X'),
+      player2: Player($(this).serializeArray()[1].value, 'O')
+    }
+    
+    game.startGame(players);
+    
+    $('.board-wrapper').html(gameBoard.getBoardHtmlString());
+    $('.reset-button').click(callbackClosureReset);
+    $('.column').click(callbackClosureColumn);
+    
+    event.preventDefault();
+  });
 });
