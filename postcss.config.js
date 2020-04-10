27dba32aa7e7876235a584918f2cd5578dbe3da6
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-04-10 17:16:29 -0300

    Fix linter errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-// It is handy to not have those transformations while we developing
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
-// const autoprefixer = require('autoprefixer');
-// const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
